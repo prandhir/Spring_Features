c1b942d3718c27c7851c46569c2d372f3d26c901
Pankaj <pankaj.randhir@gmail.com>
2018-12-18 16:03:02 -0500

    Adding Profile for first time

--- src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
+++ src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
@@ -4,16 +4,13 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-/**
- * Created by jt on 5/24/17.
- */
 @Service
 @Primary
-@Profile("en")
-public class PrimaryGreetingService implements GreetingService {
+@Profile("es")
+public class PrimarySpanishGreetingService implements GreetingService{
 
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting service";
+        return "hello From spanish profile bean";
     }
 }
